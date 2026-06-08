17e310fe2ad1b6f87fe2ecc2ed0c73712f66466b
korao <korao.fnc@gmail.com>
2026-06-08 23:35:08 +0300

    command update

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-
 import os
 from dotenv import load_dotenv
+
+
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
@@ -11,9 +12,10 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='&', intents=intents, help_command=None)
 
 
 @bot.event
@@ -25,6 +27,32 @@ async def on_ready():
 async def ping(ctx):
 
     await ctx.send('Pong!')
+    
+
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: int = None):
+    if amount is None:
+        deleted = await ctx.channel.purge(limit=2)
+        await ctx.send(f"🗑 Удалено 1 сообщение.", delete_after=3)
+        return
+    
+    deleted = await ctx.channel.purge(limit=amount + 1)
+    await ctx.send(f"🗑 Удалено {len(deleted) - 1} сообщений.", delete_after=3)
+
+@bot.command()
+@commands.has_permissions(manage_webhooks=True)
+async def create_webhook(ctx, name: str):
+    
+    webhook = await ctx.channel.create_webhook(name=name)
+    
+    webhook_url = webhook.url
+    await ctx.send(f"✅ Вебхук {name} создан! URL: {webhook_url}")
+    
+@bot.command()
+async def help(ctx):
+    await ctx.send(f"&help - показать этот список\n&clear <к-во> - удалить сообщения\n&create_webhook <название> - создать вебхук с именем")
+    return
 
 
 bot.run(TOKEN)
